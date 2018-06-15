1f8997ac4b1ff9c4f9e8360c95f7ec1683a71bef
Taylor Vories <taylor@tmtech.me>
2018-06-15 10:54:57 -0600

    trying to remove env variables before calling packer

--- update/Build-Updates.ps1
+++ update/Build-Updates.ps1
@@ -16,8 +16,10 @@ $packer_file = "02-$OSVersion-updates.json"
 # Set packer log level
 $env:PACKER_LOG=2
 
+$packer_exe = $env:packer_exe_path
+
 # Set up build
-& $env:packer_exe_path build -force -var-file="variables-global.json" -var "name=$OSVersion" -var "output_dir=$OutputDirectory" $packer_file
+& packer build -force -var-file="variables-global.json" -var "name=$OSVersion" -var "output_dir=$OutputDirectory" $packer_file
 
 if($LastExitCode -eq 0) { # Run was successful
     Write-Host "Updates were successfull for $OSVersion."
